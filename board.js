be502c37bb4746a85b429bfea789f88a61effa4f
Shishira4 <shivashishira@gmail.com>
2021-09-05 18:54:44 +0530

    changed function

--- board.js
+++ board.js
@@ -8,7 +8,6 @@ function drag(ev) {
 
 function drop(ev) {
   ev.preventDefault();
-  var data = document.createElement(''); 
-  data = ev.dataTransfer.getData("text");
+  var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementsByClassName(data));
 }
