0678d1f41334e4d2dfdfad7193e49599cd4db161
Shishira4 <shivashishira@gmail.com>
2021-09-05 19:25:24 +0530

    changed decription of cards

--- board.js
+++ board.js
@@ -11,3 +11,8 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementsByClassName(data));
 }
+function add(ev)
+{
+  var card = '<div class="card mb-3 cursor-grab" draggable="true" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)"> <div class="card-body"><p class="mb-0">You can move these elements between the containers</p></div></div></div>';
+  ev.target.appendChild(card);
+}
